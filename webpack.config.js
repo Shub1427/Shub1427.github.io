0826e87d2d3ad16bf7222e7326972a1cb518b1c2
Shub1427 <shub1493biswas@gmail.com>
2017-06-03 18:12:36 +0530

    Adding support for HBS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractText = new ExtractTextPlugin({
   filename: "[name].css"
@@ -15,17 +16,15 @@ module.exports = {
   devServer: {
     compress: true,
     port: 9000,
-    hot: true,
-    publicPath: "/public"
+    hot: true
   },
   entry: {
     index: './src/index.js'
   },
 
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: "/public"
   },
 
   resolve: {
@@ -62,12 +61,25 @@ module.exports = {
             }
           ]
         })
+      },
+      {
+        test: /\.(hbs|handlebars)$/,
+        use: [
+          {
+            loader: 'handlebars-loader'
+          }
+        ]
       }
     ]
   },
   plugins: [
     extractText,
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Something Else...',
+      filename: 'index.html',
+      template: 'src/index.hbs'
+    })
   ]
 }
