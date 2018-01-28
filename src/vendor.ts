e6f393b10e36ff3d41e99b01db3e4ea2e0d45e3a
Subroto <subrotob@azuga.com>
2018-01-28 19:57:59 +0530

    Updates

--- src/vendor.ts
+++
@@ -1,7 +0,0 @@
-import '@angular/platform-browser';
-import '@angular/core';
-import '@angular/platform-browser-dynamic';
-import '@angular/common';
-import '@angular/forms';
-import 'rxjs/Rx';
-import 'immutable';
