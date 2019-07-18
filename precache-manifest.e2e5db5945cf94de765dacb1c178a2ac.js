9ebd9203f3fb4fe5c9f453b97cfea919dff86e6a
Shub1427 <shub1493biswas@gmail.com>
2019-07-18 22:33:21 +0530

    Updates

---
+++ precache-manifest.e2e5db5945cf94de765dacb1c178a2ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fe5cb8e97a0b43ebdf77c5bfa71209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8e1336924d6a3fbdd6a",
+    "url": "/static/css/main.0d95d863.chunk.css"
+  },
+  {
+    "revision": "7f759540c5b130cf0447",
+    "url": "/static/js/2.7de6a1c7.chunk.js"
+  },
+  {
+    "revision": "d8e1336924d6a3fbdd6a",
+    "url": "/static/js/main.4cf2f757.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c2fda93ad05da8f8ca63eb464181aaa",
+    "url": "/static/media/profile.2c2fda93.png"
+  }
+]);
