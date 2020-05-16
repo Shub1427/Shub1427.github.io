cfe6857372f4288418db725844a6ac07e98038c5
Shub1427 <shub1493biswas@gmail.com>
2020-05-16 10:39:00 +0530

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fc3af2e7a57df443c136";
+/******/ 	var hotCurrentHash = "2a7bcee8cb948268814e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
